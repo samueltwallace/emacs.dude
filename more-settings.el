dd908f6d2aa7708fe6f9422f5529a93aee0eda3f
Samuel Wallace <swalla24@uic.edu>
2022-04-26 14:03:18 -0500

    finally done with overhaul

--- more-settings.el
+++ more-settings.el
@@ -1,3 +1,7 @@
+(defun add-menu-item (key command)
+       (global-set-key (kbd (concat "C-; " key)) command))
+(defun find-init-file () (interactive) (find-file (user-init-file))
+
 (setq custom-file "~/.emacs.d/custom.el")
 (load-file custom-file)
 
@@ -53,9 +57,9 @@
   (setq org-agenda-start-on-weekday 0)
   (require 'org-ref-ivy)
   (setq org-ref-insert-link-function 'org-ref-insert-link-hydra/body
- org-ref-insert-cite-function 'org-ref-cite-insert-ivy
- org-ref-insert-label-function 'org-ref-insert-label-link
- org-ref-insert-ref-function 'org-ref-insert-ref-link)
+	org-ref-insert-cite-function 'org-ref-cite-insert-ivy
+	org-ref-insert-label-function 'org-ref-insert-label-link
+	org-ref-insert-ref-function 'org-ref-insert-ref-link)
   (require 'org-ref)
   (require 'org-noter)
   (setq org-todo-keywords
@@ -70,71 +74,70 @@
   :defer t
   :config (org-init-settings))
 
-(defun init-mail-settings () ()
-  (setq
-   mu4e-get-mail-command "offlineimap -q -o"
-   mu4e-update-interval 30000))
+(add-menu-item "a" 'org-agenda)
 
-(use-package mu4e
-  :load-path  "/usr/share/emacs/site-lisp/mu4e"
-  :init (init-mail-settings))
+(setq bibtex-completion-pdf-field "file"
+      bibtex-completion-library-path '("~/pdfs"))
 
-(add-hook 'mu4e-compose-mode-hook 'turn-off-auto-fill)
-(add-hook 'LaTeX-mode-hook 'latex-hook)
-(add-hook 'org-mode-hook 'org-hook)
+(add-menu-item "b" 'ivy-bibtex)
 
-(defun file-manipulation-settings () (interactive)
-       (progn
-	 (use-package projectile)
-	 (use-package magit)
-	 (use-package dired-x)
-	 (projectile-mode +1)
-	 (setq dired-listing-switches "-ahl")
-	 (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
-	 (setq dired-guess-shell-alist-user
-	       '(("\\.bib$" "/home/samueltwallace/.local/bin/bibly")
-		 ("\\.pdf$" "okular *")))))
-
-(defun zotero-store () (interactive)
-       (find-dired "~/Zotero/storage" "-name '*.pdf'"))
-(defun doc-store () (interactive)
-       (find-dired "~/Documents/Textbooks" "name '*.pdf'"))
-
-(defun smart-kill-word () (interactive)
-  (forward-word)
-  (kill-word -1))
-
-(setq sentence-end-double-space nil)
-
-(defun editing-settings () (interactive)
-       (use-package counsel)
-       (use-package crux)
-       (ivy-mode +1)
-       (counsel-mode +1)
-       (counsel-projectile-mode +1)
-       (setq bibtex-completion-bibliography '("~/zoterolib.bib"))
-       (global-set-key (kbd "C-x s") 'swiper)
-       (global-set-key (kbd "C-k") 'crux-smart-kill-line)
-       (global-set-key (kbd "M-d") 'smart-kill-word))
-
-(global-set-key (kbd "C-c v v") 'view-mode)
-(global-set-key (kbd "C-c v f") 'follow-mode)
-(unbind-key "C-z")
 (defun machine-uptime () (interactive) (shell-command "uptime"))
 (defun pacman-update () (interactive) (async-shell-command "sudo pacman -Syu"))
 (defun get-weather () (interactive)
        (async-shell-command "curl -s 'https://wttr.in/chicago?0p'" "*wttr.in*" nil))
-(global-set-key (kbd "C-z p") 'ping)
-(global-set-key (kbd "C-z t") 'machine-uptime)
-(global-set-key (kbd "C-z b") 'battery)
-(global-set-key (kbd "C-z u") 'pacman-update)
-(global-set-key (kbd "C-z w") 'get-weather)
 
-(global-set-key (kbd "M-o") 'ace-window)
-(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
-(global-set-key (kbd "M-s m") 'counsel-imenu)
-(global-set-key (kbd "M-s b") 'counsel-ibuffer)
-(global-set-key (kbd "M-z") 'counsel-linux-app)
+(defun smart-kill-word () (interactive)
+       (foward-word)
+       (kill-word -1))
+
+(defun my-editing-keybindings () (interactive)
+       (setq sentence-end-double-space nil)
+       (global-set-key (kbd "C-x s") 'swiper)
+       (global-set-key (kbd "C-k") 'crux-smart-kill-line)
+       (global-set-key (kbd "M-d") 'smart-kill-word)
+       (unbind-key "C-z")
+       (global-set-key (kbd "C-z p") 'ping)
+       (global-set-key (kbd "C-z t") 'machine-uptime)
+       (global-set-key (kbd "C-z b") 'battery)
+       (global-set-key (kbd "C-z u") 'pacman-update)
+       (global-set-key (kbd "C-z w") 'get-weather)
+	  )
+
+(use-package magit)
+
+(use-package projectile
+:config (progn
+	  (projectile-mode 1)
+	  (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map))
+
+(use-package dired-x
+:config (progn
+	  (setq dired-listing-switches "-ahl")
+	  (setq dired-guess-shell-alist-user
+		'(("\\.bib$" "~/.local/bin/bibly")
+		  ("\\.pdf$" "okular *")))
+
+(use-package counsel
+:config (progn
+	  (counsel-mode 1)
+	  (counsel-projectile-mode 1)
+	  (ivy-mode 1)
+	  (counsel-keybindings)))
+
+(use-package crux
+:config (my-editing-keybindings))
+
+(repeat-mode)
+
+(defun counsel-keybindings () (interactive)
+       (global-set-key (kbd "M-o") 'ace-window)
+       (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+       (global-set-key (kbd "M-s m") 'counsel-imenu)
+       (global-set-key (kbd "M-s b") 'counsel-ibuffer)
+       (global-set-key (kbd "M-z") 'counsel-linux-app))
+
+(use-package avy
+:config (my-avy-keybindings))
 
 (defun lock-screen-with-slock () (interactive) (call-process "slock"))
 
@@ -213,23 +216,12 @@
 (defun current-window-layout () (interactive)
        (message split-window-preferred-function))
 
-(use-package avy) 
+(defun my-avy-keybindings () (interactive)
 (global-set-key (kbd "M-g g") 'avy-goto-char-2)
 (global-set-key (kbd "M-g c") 'avy-goto-char)
 (global-set-key (kbd "M-g M-g") 'avy-goto-line)
 (global-set-key (kbd "M-g f") 'avy-goto-char-in-line)
-(global-set-key (kbd "M-g e") 'avy-goto-end-of-line)
-
-(defun add-menu-item (key command)
-       (global-set-key (kbd (concat "C-; " key)) command))
-(defun find-init-file () (interactive) (find-file "~/.emacs.d/index.org"))
-(add-menu-item "m" 'mu4e)
-(add-menu-item "i" 'find-init-file)
-(add-menu-item "a" 'org-agenda)
-(add-menu-item "p" 'proced)
-(add-menu-item "b" 'ivy-bibtex)
-
-(repeat-mode)
+(global-set-key (kbd "M-g e") 'avy-goto-end-of-line))
 
 (defvar make-window-repeat-map
   (let ((map (make-sparse-keymap)))
@@ -317,8 +309,7 @@
   :lighter " cum"
   :keymap cumulative-map)
 
-(use-package elfeed
-  :ensure t)
+(defun my-elfeed-settings () (interactive)
 (global-set-key (kbd "C-; e") 'elfeed)
 (setq elfeed-feeds
       '(("http://arxiv.org/rss/math.AP" preprint)
@@ -327,9 +318,32 @@
 	("https://planet.emacslife.com/atom.xml" emacs)
 	("https://kbd.news/rss2.php" keyboard)
 	("https://sachachua.com/blog/feed/" emacs)
-	))
+	)))
 
-(defun wiki-search (search-term) (interactive "sSearch Wikipedia: ") (browse-url (concat "https://en.wikipedia.org/w/index.php?title=Special%3ASearch&search=" search-term)))
+(use-package elfeed
+:config (my-elfeed-settings))
 
 (use-package pdf-tools
   :config (pdf-tools-install))
+
+(defun wiki-search (search-term) (interactive "sSearch Wikipedia: ") (browse-url (concat "https://en.wikipedia.org/w/index.php?title=Special%3ASearch&search=" search-term)))
+
+(defun init-mail-settings () ()
+  (setq
+   mu4e-get-mail-command "offlineimap -q -o"
+   mu4e-update-interval 30000))
+
+(use-package mu4e
+  :load-path  "/usr/share/emacs/site-lisp/mu4e"
+  :init (init-mail-settings))
+
+(add-menu-item "m" 'mu4e)
+
+(add-menu-item "m" 'mu4e)
+(add-menu-item "a" 'org-agenda)
+(add-menu-item "p" 'proced)
+(add-menu-item "b" 'ivy-bibtex)
+
+(add-hook 'mu4e-compose-mode-hook 'turn-off-auto-fill)
+(add-hook 'LaTeX-mode-hook 'latex-hook)
+(add-hook 'org-mode-hook 'org-hook)
